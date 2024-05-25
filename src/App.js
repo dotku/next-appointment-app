4594fc64bf06aa06eb99c73e4d6b2545626052a3
Weijing Jay lin <weijingjaylin@gmail.com>
2024-05-24 17:32:43 -0700

    clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Contacts from "./data/dummyContact.json";
 import BookingPage from "./components/Customer/BookingPage";
 import Header from "./components/Common/Header";
-
-function Cell({ row }) {
-  const objectKeys = Object.keys(row);
-  return objectKeys.map((key) => <td key={key}>{row[key]}</td>);
-}
 
 function App() {
   return (
