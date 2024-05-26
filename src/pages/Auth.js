3d39a4282c1cd30e4f88a4677dabfd7c0ee6a724
Weijing Jay lin <weijingjaylin@gmail.com>
2024-05-26 01:33:04 -0700

    update env name

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -2,8 +2,8 @@ import { createClient } from "@supabase/supabase-js";
 import { Auth } from "@supabase/auth-ui-react";
 
 const supabase = createClient(
-  process.env.REACT_NEXT_PUBLIC_SUPABASE_URL,
-  process.env.REACT_NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 const AppAuth = () => <Auth supabaseClient={supabase} />;
