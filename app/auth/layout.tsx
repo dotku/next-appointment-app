728c32aeccc04bedcf71d98ab7a03a56f4e26535
Weijing Jay lin <weijingjaylin@gmail.com>
2024-06-16 15:35:31 -0700

    simplify navbar for auth page

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import AppNavbar from "../../src/components/Common/Navbar/AppNavbar";
+import { Link, Navbar, NavbarBrand } from "@nextui-org/react";
 
 export const metadata: Metadata = {
   title: "Appointment Booking App",
@@ -14,7 +15,13 @@ export default function AppointmentLayout({
 }) {
   return (
     <>
-      <AppNavbar slug="auth" />
+      <Navbar>
+        <NavbarBrand className="flex justify-center">
+          <Link className="font-bold text-inherit" href="/">
+            AptApp
+          </Link>
+        </NavbarBrand>
+      </Navbar>
       <div className="container mx-auto px-6">{children}</div>
     </>
   );
