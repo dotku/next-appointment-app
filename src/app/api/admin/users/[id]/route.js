7f991a2197cacecd858072dd02b7a7a567118858
Weijing Jay lin <weijingjaylin@gmail.com>
2024-06-23 21:28:14 -0700

    cleanup code

--- src/app/api/admin/users/[id]/route.js
+++ src/app/api/admin/users/[id]/route.js
@@ -1,12 +1,7 @@
-// app/api/users/route.js
-
 import { supabaseAdmin } from "@/src/services/supabaseAdmin";
-// import type { NextApiRequest } from "next";
 
 export async function GET(_req, { params }) {
   const { data, error } = await supabaseAdmin.auth.admin.getUserById(params.id);
-  // const router = useRouter();
-  console.log("router", params);
 
   if (error) {
     return new Response(JSON.stringify({ error: error.message }), {
